7e28641aa345e5c5960303c4c0560144832be284
kosuke <kosuke.uchida.t@gmail.com>
2025-04-27 18:35:55 +0900

    Remove cxx folder from repository

--- cxx/main/main.cxx
+++
@@ -1,9 +0,0 @@
-#include"../classes/machineLearning/regression/elasticNetLinearRegression/elasticNetLinearRegression.h"
-
-int main(void){
-    Matrix data("red_wine.txt");
-    ElasticNetLinearRegression test(data,0.5,0,0.1,0.1);
-    test.train(1.0E-16);
-    test.printMae();
-    return 0;
-}
